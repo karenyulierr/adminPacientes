b0a9403f88f1b6ce3894ffc0ab8ddf960871d41a
Karen Rodriguez <karenrodriguez@artistshot.com>
2023-06-23 14:45:46 -0500

    Primeros pasos con Tailwind y el Proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
+
+  import Header from './components/Header.vue';
+
 </script>
 
 <template>
-  <h1 class="text-6xl ">Hola mundo </h1>
+  <div class="container mx-auto mt-20">
+    <Header/>
+  </div>
 </template>
