d270031c65a92c3637d77123a28854919b963cb9
Karen Rodriguez <karenrodriguez@artistshot.com>
2023-06-23 14:52:32 -0500

    Creando el componente para el formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade pacientes y
+            <span class="text-indigo-600 font-bold">
+                Adminístralos
+            </span>
+        </p>
+    </div>
+</template>
