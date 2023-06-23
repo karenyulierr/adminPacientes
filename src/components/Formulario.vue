a8aa36be25f9a52ff79676796ae3a6dbdb5d68ad
Karen Rodriguez <karenrodriguez@artistshot.com>
2023-06-23 15:13:02 -0500

    Finalizando el formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -11,5 +11,33 @@
                 Adminístralos
             </span>
         </p>
+
+        <form action="" class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <div class="mb-5">
+                <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+                <input type="text" id="mascota" placeholder="Nombre de la mascota" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre propietario</label>
+                <input type="text" id="propietario" placeholder="Nombre del propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="email" class="block text-gray-700 uppercase font-bold">Email del propietario</label>
+                <input type="email" id="email" placeholder="Email del propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
+                <input type="date" id="alta" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">Síntomas</label>
+                <textarea Type="text" id="sintomas" placeholder="Describe los síntomas" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40" />
+            </div>
+
+            <input type="submit"
+                   class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" 
+                   value="Registrar paciente"
+            >
+        </form>
     </div>
 </template>
